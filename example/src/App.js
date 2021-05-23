075d8c969628f8e53d413b1f7e7d522fddfb1910
polo44 <sespolo@gmail.com>
2021-05-23 18:03:10 +0100

    added infinite scroll

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,27 @@
 import React, { useEffect, useState } from 'react'
 
 import DataGrid from 'mui-data-datagrid'
+import { Box, makeStyles, Typography, useTheme } from '@material-ui/core'
+
+const useStyles = makeStyles(theme => ({
+
+  DGridHeaderCell: {
+    color: `${theme.palette.primary.main}66`
+  },
+  DGridCell: {
+    backgroundColor: theme.palette.background.paper,
+    borderBottom: `1px solid ${theme.palette.primary.main}66`
+  }
+}))
 
 function App() {
 
+  const classes = useStyles()
+  const theme = useTheme()
+
   const [rows, setRows] = useState([])
+  const [hasMore, setHasMore] = useState(true)
+  const [loadingMore, setLoadingMore] = useState(false)
 
   const columns = [
     { id: '_Name', isNumeric: false, label: "Name", main: true },
@@ -17,11 +34,58 @@ function App() {
     alert('Hi !')
   }
 
-  useEffect(() => {
+  const handleInfiniteScrollClick = () => {
+    alert('I was added by infinite scroll')
+  }
+
+  const loadMoreAction = () => {
+
+
+    // if (rows.length <= 0) return
+
+    // alert('started')
+    console.log('loading')
+    setLoadingMore(true)
+
+    setTimeout(() => {
+
+      let data = []
+      for (let index = 0; index < 10000; index++) {
+        data = rows.length <= 0 ?
+          [...data, {
+            _id: Math.floor(Math.random() * 9999999999),
+            _Name: 'test.png', _Visibility: 'Public',
+            _Attributes: {
+              _ReadOnly: 'No'
+            },
+            actions: <button onClick={handleOnClick}>Hi !</button>
+          }]
+          :
+          [...data, {
+            _id: Math.floor(Math.random() * 9999999999),
+            _Name: 'InfiniteScrollRow.png', _Visibility: 'Public',
+            _Attributes: {
+              _ReadOnly: 'Yes'
+            },
+            actions: <button onClick={handleInfiniteScrollClick}>I'm new !</button>
+          }]
+      }
+
+      console.log('done')
+      setLoadingMore(false)
+      setHasMore(true)
+      setRows(rows.concat(data))
+    }, 3000)
+
+
+  }
+
+  /* useEffect(() => {
 
     let data = []
-    for (let index = 0; index < 1000; index++) {
+    for (let index = 0; index < 50; index++) {
       data = [...data, {
+        _id: Math.floor(Math.random() * 9999999999),
         _Name: 'test.png', _Visibility: 'Public',
         _Attributes: {
           _ReadOnly: 'No'
@@ -29,14 +93,22 @@ function App() {
         actions: <button onClick={handleOnClick}>Hi !</button>
       }]
     }
+    setHasMore(true)
     setRows(data)
-  }, [])
+  }, []) */
 
   return (
-    <div style={{ width: '100vw', height: '100vh', backgroundColor: '#F8F9FF' }}>
-      <DataGrid
-        columns={columns} rows={rows}
-      />
+    <div style={{ display: 'flex', flexDirection: 'column', width: '100%', height: '100vh', backgroundColor: '#F8F9FF' }}>
+
+      <Typography style={{ margin: theme.spacing(2) }}>{rows.length} rows</Typography>
+      <Box display="flex" flexDirection="column" flexGrow={1} >
+        <DataGrid
+          columns={columns} rows={rows}
+          headerCellClass={classes.DGridHeaderCell} cellClass={classes.DGridCell}
+          headerHeight={60} rowHeight={100} rowSpacing={20}
+          loadMoreFunc={loadMoreAction} hasMore={hasMore} loadingMore={loadingMore}
+        />
+      </Box>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import styles from './styles.module.css'
-
 import React, { useState } from 'react'
-import { Box, makeStyles, TableSortLabel, Typography, useTheme } from '@material-ui/core'
+import { Fragment } from 'react'
+import PropTypes from 'prop-types'
+import { Box, CircularProgress, makeStyles, TableSortLabel, Typography, useTheme } from '@material-ui/core'
 
 // Libs
 import { VariableSizeGrid as DGrid } from 'react-window'
-import AutoSizer from "react-virtualized-auto-sizer";
-import { Fragment } from 'react';
+import AutoSizer from "react-virtualized-auto-sizer"
+import InfiniteLoader from "react-window-infinite-loader"
 
 const scrollbarWidth = 10
+const inifiniteLoaderKey = `ifnl-${Math.floor(Math.random() * 9999999999)}`
 
 const useStyles = makeStyles(theme => ({
   visuallyHidden: {
@@ -30,13 +31,22 @@ const useStyles = makeStyles(theme => ({
     },
     '&::-webkit-scrollbar': {
       width: scrollbarWidth,
+      height: scrollbarWidth,
       backgroundColor: "#F5F5F5"
     },
     '&::-webkit-scrollbar-thumb': {
       width: scrollbarWidth,
+      height: scrollbarWidth,
       borderRadius: scrollbarWidth,
       backgroundColor: theme.palette.primary.main
     }
+  },
+  DGridHeaderCell: {
+    color: `${theme.palette.primary.dark}66`
+  },
+  DGridCell: {
+    backgroundColor: theme.palette.background.paper,
+    borderBottom: `1px solid ${theme.palette.primary.main}66`
   }
 }))
 
@@ -85,7 +95,7 @@ function HeaderCellRenderer({ columnIndex, data, style }) {
 
 
   return (
-    <Box display="flex" alignItems="center" style={{ ...style, ...data.headerStyle, height: data.headerHeight }}>
+    <Box display="flex" alignItems="center" className={data.headerCellClass} style={{ ...style, height: data.headerHeight }}>
       <Box pl={2}>
         <Typography variant="body2" >
           <TableSortLabel
@@ -124,9 +134,13 @@ function CellRenderer({ columnIndex, data, rowIndex, style }) {
         :
 
         // Body rows
-        <Box display="flex" alignItems="center" style={{ ...style, ...data.style, height: data.rowHeight - data.rowSpacing }}>
+        <Box display="flex" alignItems="center" className={data.className} style={{ ...style, height: data.rowHeight - data.rowSpacing }}>
           <Box pl={2}>
-            {handleColumnID(data.data[rowIndex - 1], data.columns[columnIndex].id)}
+            {data.isItemLoaded(rowIndex) ?
+              handleColumnID(data.data[rowIndex - 1], data.columns[columnIndex].id)
+              :
+              columnIndex === 0 && <CircularProgress size={20} />
+            }
           </Box>
           <Box height={data.rowSpacing} />
         </Box>
@@ -136,13 +150,58 @@ function CellRenderer({ columnIndex, data, rowIndex, style }) {
 
 }
 
-
-export default function DataGrid({ columns = [], rows = [],
-  order, orderBy,
-  headerHeight = 60, rowHeight = 100, rowSpacing = 20,
-  gridStyle = null, headerCellStyle = null, cellStyle = null }) {
+function InfiniteScroller({ loadMoreFunc, hasMore, loadingMore, rowsCount, children }) {
 
   const theme = useTheme()
+
+  // If there are more items to be loaded then add an extra row to hold a loading indicator.
+  const itemCount = hasMore ? rowsCount + 1 : rowsCount;
+
+  // Only load 1 page of items at a time.
+  // Pass an empty callback to InfiniteLoader in case it asks us to load more than once.
+  const loadMoreItems = loadingMore ? () => { } : loadMoreFunc;
+
+  // Every row is loaded except for our loading indicator row.
+  const isItemLoaded = index => !hasMore || index < rowsCount;
+
+  return (
+    <>
+      {loadMoreFunc ?
+        <InfiniteLoader
+          isItemLoaded={isItemLoaded}
+          itemCount={itemCount}
+          loadMoreItems={loadMoreItems}
+        >
+          {({ onItemsRendered, ref }) => (
+            children
+          )}
+        </InfiniteLoader>
+        :
+        children
+      }
+    </>
+  )
+}
+
+export default function DataGrid({
+
+  // Data props
+  columns = [], rows = [],
+
+  // Sort props
+  order, orderBy,
+
+  // Size props
+  headerHeight = 60, rowHeight = 100, rowSpacing = 20,
+
+  // Styling props
+  gridStyle = null, headerCellClass = null, cellClass = null,
+
+  // Infinite scroll props
+  loadMoreFunc = null, hasMore = false, loadingMore = false
+}) {
+
+  // const theme = useTheme()
   const classes = useStyles()
 
   const [sort, setSort] = useState(order)
@@ -178,47 +237,109 @@ export default function DataGrid({ columns = [], rows = [],
   const columnsWidth = getColumnsWidth()
 
 
+  // Infinite scroll manager
+
+  const rowsCount = rows.length
+  // If there are more items to be loaded then add an extra row to hold a loading indicator.
+  const itemCount = hasMore ? rowsCount + 1 : rowsCount;
+
+  // Only load 1 page of items at a time.
+  // Pass an empty callback to InfiniteLoader in case it asks us to load more than once.
+  const loadMoreItems = loadingMore ? () => { } : loadMoreFunc;
+
+  // Every row is loaded except for our loading indicator row.
+  const isItemLoaded = index => !hasMore || index < rowsCount;
+
 
   return (
+
     <AutoSizer>
+      {/* <InfiniteScroller loadMoreFunc={loadMoreFunc} hasMore={hasMore}> */}
       {({ height, width }) => (
-        <DGrid
-          // className="Grid"
-          columnCount={columns.length}
-          columnWidth={index => {
+        <InfiniteLoader isItemLoaded={isItemLoaded} itemCount={itemCount} loadMoreItems={loadMoreItems}>
+          {({ onItemsRendered, ref }) => {
 
-            return (columnsWidth.total < width) ?
-              columnsWidth.data[index].width + ((width - columnsWidth.total) / columns.length) - (scrollbarWidth / columns.length)
-              : columnsWidth.data[index].width
+            const newItemsRendered = ({
+              visibleRowStartIndex,
+              visibleRowStopIndex,
+              visibleColumnStopIndex,
+              overscanRowStartIndex,
+              overscanRowStopIndex,
+              overscanColumnStopIndex
+            }) => {
+              const useOverscanForLoading = true
+
+              const endCol =
+                (useOverscanForLoading || true
+                  ? overscanColumnStopIndex
+                  : visibleColumnStopIndex) + 1;
+
+              const startRow =
+                useOverscanForLoading || true
+                  ? overscanRowStartIndex
+                  : visibleRowStartIndex;
+              const endRow =
+                useOverscanForLoading || true
+                  ? overscanRowStopIndex
+                  : visibleRowStopIndex;
+
+              const visibleStartIndex = startRow;
+              const visibleStopIndex = endRow;
+
+              onItemsRendered({
+                //call onItemsRendered from InfiniteLoader so it can load more if needed
+                visibleStartIndex,
+                visibleStopIndex
+              })
+            }
+
+            return (
+              <DGrid
+                onItemsRendered={newItemsRendered}
+                ref={ref}
+
+                columnCount={columns.length}
+                columnWidth={index => {
+
+                  return (columnsWidth.total < width) ?
+                    columnsWidth.data[index].width + ((width - columnsWidth.total) / columns.length) - (scrollbarWidth / columns.length)
+                    : columnsWidth.data[index].width
+                }}
+                height={height}
+                rowCount={rows.length + 1}
+                rowHeight={index => (index === 0 ? headerHeight : rowHeight)}
+                width={width}
+                style={!gridStyle ? {
+
+                } : gridStyle}
+                // className="d-grid"
+                className={classes.DGridScrollbar}
+
+                itemData={{
+                  data: stableSort(rows, getComparator(sort, sortBy)), columns: columns, rowHeight: rowHeight, rowSpacing: rowSpacing,
+                  headerHeight: headerHeight,
+                  sort: sort, sortBy: sortBy, handleRequestSort: handleRequestSort,
+                  className: !cellClass ? classes.DGridCell : cellClass,
+                  headerCellClass: !headerCellClass ? classes.DGridHeaderCell : headerCellClass,
+
+                  isItemLoaded: isItemLoaded
+                }}
+                itemKey={({ columnIndex, data, rowIndex }) => `${data.columns[columnIndex].id} | ${rowIndex === 0 ? '1' : (data.data[rowIndex - 1]._id)}`}
+              >
+                {CellRenderer}
+              </DGrid>
+            )
           }}
-          height={height}
-          rowCount={rows.length + 1}
-          rowHeight={index => (index === 0 ? headerHeight : rowHeight)}
-          width={width}
-          style={!gridStyle ? {
 
-          } : gridStyle}
-          // className="d-grid"
-          className={classes.DGridScrollbar}
-
-          itemData={{
-            data: stableSort(rows, getComparator(sort, sortBy)), columns: columns, rowHeight: rowHeight, rowSpacing: rowSpacing,
-            headerHeight: headerHeight,
-            sort: sort, sortBy: sortBy, handleRequestSort: handleRequestSort,
-            style: !cellStyle ? {
-              backgroundColor: theme.palette.background.paper,
-              borderBottom: `1px solid ${theme.palette.primary.main}66`
-            } : cellStyle,
-            headerStyle: !headerCellStyle ? {
-              color: `${theme.palette.primary.dark}66`
-            } : headerCellStyle
-          }}
-          itemKey={({ columnIndex, data, rowIndex }) => `${data.columns[columnIndex].id} | ${rowIndex === 0 ? '1' : (data.data[rowIndex - 1]._id)}`}
-        >
-          {CellRenderer}
-        </DGrid>
+        </InfiniteLoader>
       )
       }
     </AutoSizer >
   )
+}
+
+DataGrid.propTypes = {
+  headerHeight: PropTypes.number,
+  rowHeight: PropTypes.number,
+  rowSpacing: PropTypes.number,
 }
